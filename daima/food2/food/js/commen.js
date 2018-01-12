0e3e2314dfcd49732509c9eb2c45b7ac84290414
caixiaoru <1441780228@qq.com>
2018-01-12 17:06:06 +0800

    添加 Signed-off-by: caixiaoru <1441780228@qq.com>

--- daima/food2/food/js/commen.js
+++ daima/food2/food/js/commen.js
@@ -3,6 +3,16 @@ $(function(){
     $(".my_back").click(function(){
         $(window).attr('location','./my.html');
     });
+    $(".store_back").click(function(){
+        $(window).attr('location','./index.html');
+    });
+    $(".activity_back").click(function(){
+        $(window).attr('location','./index.html');
+    });
+    $(".style_back").click(function(){
+        $(window).attr('location','./index.html');
+    });
+
     //首页搜索
     $(".index_search").focus(function(){
     $(window).attr('location','./search.html');
@@ -29,9 +39,9 @@ $(function(){
     });
 //健康生活展开收起
     console.log(333);
-    if($(".life_main_list_content_two_a").height()>52){
+    if($(".life_main_list_content_two_a").height()>55){
         //如果文字超过三行，设置div高度为三行，并显示展开按钮
-        $(".life_main_list_content_two_a").css("height",52);
+        $(".life_main_list_content_two_a").css("height",55);
         //显示展开按钮
         $(".life_main_list_content_two p:nth-of-type(2)").css("display","block");
         //点击展开按钮，显示内容全部
@@ -52,6 +62,40 @@ $(function(){
 
     }
 
+//商家店铺store.html
+    $(".store_main_food_left ul li").click(function(){
+        console.log("左侧");
+        $(this).addClass("store_main_food_left_active").siblings().removeClass("store_main_food_left_active");
+        //$(".store_main_food_right ul li").eq($(this).index()).addClass("store_main_food_right_show").siblings().removeClass("store_main_food_right_show");
+    });
+
+
+
+//    点击收藏
+    $(".store_collect").click(function(){
+
+
+        if($(this).hasClass("fa-heart-o")){
+            $(this).attr("class","fa fa-heart fa-2x store_collect");
+        }
+        else{
+            $(this).attr("class","fa fa-heart-o fa-2x store_collect");
+        }
+    })
 
 
 });
+$(window).scroll(function(){
+
+    //        console.log($(".store_food_style").offset().top);
+    console.log($(window).scrollTop());
+    ////获取分类据顶部的高度
+    var a=$("#store_header").height();
+    var b=$(".store_main").height();
+    var c=a+b+15;
+
+    if($(window).scrollTop()> c){
+        $(".store_food_style").css({"position":"fixed","top":0});
+    }
+    }
+)
